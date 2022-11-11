c4c0e6d07fb5f369664c1c5d186e3de33983a44c
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2022-11-11 01:28:38 -0500

    no publish for now 😭(i published too many)

--- tools/release.sh
+++ tools/release.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cargo release --allow-branch main -p alloy-core --all-features --tag-name 'v{{version}}' -v -x $1
+cargo release --allow-branch main -p alloy-core --all-features --tag-name 'v{{version}}' --no-publish -v -x $1
